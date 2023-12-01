34cfedb64180fb6acca4b434dd1a190ea98284a7
Karne <cadalo.karen@gmail.com>
2023-12-01 19:28:15 +0800

    Third Commit For Last Today's Video

--- lib/home.dart
+++ lib/home.dart
@@ -24,120 +24,100 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text('Form Handling'),
-      ),
-      body: Form(
-        key: formKey,
-        child: ListView(
-          padding: const EdgeInsets.all(20),
-          children: [
-            TextFormField(
-              controller: fbProfileLink,
-              keyboardType: TextInputType.url,
-              decoration: const InputDecoration(
-                  labelText: "Facebook Profile Link"
-              ),
-              validator: (value){
-                if (value == null || value == ''){
-                  return "Please enter your fb profile link";
-                }
-
-                if(!value.isUrl()){
-                  return "Please enter your fb profile link";
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: usernameController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                labelText: "Email Address"
-
-              ),
-              validator: (value){
-                if (value == null || value == ''){
-                  return "Please enter your username";
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: passwordController,
-              keyboardType: TextInputType.number,
-              obscureText: hidePassword,
-              decoration: InputDecoration(
-                labelText: "Password",
-                suffix: IconButton(
-                  onPressed: (){
-                    setState(() {
-                      hidePassword = !hidePassword;
-                    });
-                  },
-                  icon: Icon(hidePassword ? CupertinoIcons.eye : CupertinoIcons.eye_slash),
-                )
-              ),
-              validator: (value){
-                if (value == null || value == ''){
-                  return "Please enter your password";
-                }
-
-                if(value.length < 8){
-                  return "Password must be at least 8 characters long";
-                }
-                return null;
-              },
-            ),
-            DropdownButtonFormField(
-                items: [
-                  ...genders.map((gender) => DropdownMenuItem(
-                    value: gender,
-                      child: Text(gender)
-                  ))
-                ],
-                value: selectedGender,
-                onChanged: (value){
-                  selectedGender = value!;
-                }),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 20),
-              child: Row(
-                children: [
-                  Text("Gender"),
-                  Expanded(
-                    child: Container(),
-                  ),
-                  Expanded(
-                      child: Row(
-                        children: [
-                          TextButton(
-                              onPressed: (){
-
-                              },
-                              child: Text("Female")
-                          ),
-                          TextButton(
-                              onPressed: (){
-
-                              },
-                              child: Text("Male")
-                          ),
-                        ],
-                      ))
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-                onPressed: (){
-                 formKey.currentState?.validate();
-                },
-                child: Text("Login"))
-          ],
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text('Form Handling'),
         ),
-      )
+        body: Form(
+          key: formKey,
+          child: ListView(
+            padding: const EdgeInsets.all(20),
+            children: [
+              TextFormField(
+                controller: fbProfileLink,
+                keyboardType: TextInputType.url,
+                decoration: const InputDecoration(
+                    labelText: "Facebook Profile Link"
+                ),
+                validator: (value){
+                  if (value == null || value == ''){
+                    return "Please enter your fb profile link";
+                  }
+
+                  if(!value.isUrl()){
+                    return "Please enter your fb profile link";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: usernameController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                    labelText: "Email Address"
+
+                ),
+                validator: (value){
+                  if (value == null || value == ''){
+                    return "Please enter your username";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: passwordController,
+                keyboardType: TextInputType.number,
+                obscureText: hidePassword,
+                decoration: InputDecoration(
+                    labelText: "Password",
+                    suffix: IconButton(
+                      onPressed: (){
+                        setState(() {
+                          hidePassword = !hidePassword;
+                        });
+                      },
+                      icon: Icon(hidePassword ? CupertinoIcons.eye : CupertinoIcons.eye_slash),
+                    )
+                ),
+                validator: (value){
+                  if (value == null || value == ''){
+                    return "Please enter your password";
+                  }
+
+                  if(value.length < 8){
+                    return "Password must be at least 8 characters long";
+                  }
+                  return null;
+                },
+              ),
+              DropdownButtonFormField(
+                  items: [
+                    ...genders.map((gender) => DropdownMenuItem(
+                        value: gender,
+                        child: Text(gender)
+                    ))
+                  ],
+                  value: selectedGender,
+                  onChanged: (value){
+                    selectedGender = value!;
+                  }),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                  onPressed: (){
+                    print('Facebook Profile Link: ${fbProfileLink.text}');
+                    print('Email Address: ${usernameController.text}');
+                    print('Password: ${passwordController.text}');
+                    print('Email: ${selectedGender}');
+
+                    fbProfileLink.text = "";
+                    usernameController.text = "";
+                    passwordController.text = "";
+                    formKey.currentState?.validate();
+                  },
+                  child: Text("Login"))
+            ],
+          ),
+        )
     );
   }
 }
